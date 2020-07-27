19e159ce1975f6e03d4da44d3aaecd466b19952d
tomas-burotto <tomas.burotto@beetrack.com>
2020-07-27 01:10:43 -0400

    Updates

---
+++ precache-manifest.f98ff4c56cd51f7b62503894c3bc61ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998f54b2a2e5c443b586270d2c170a35",
+    "url": "/movies-front/index.html"
+  },
+  {
+    "revision": "d4543f663905a035a466",
+    "url": "/movies-front/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d692e26a323a6da1a9c6",
+    "url": "/movies-front/static/js/2.f0b63859.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/movies-front/static/js/2.f0b63859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4543f663905a035a466",
+    "url": "/movies-front/static/js/main.9a709845.chunk.js"
+  },
+  {
+    "revision": "9a44dbe5cbfc04596f5b",
+    "url": "/movies-front/static/js/runtime-main.bd7212be.js"
+  }
+]);
